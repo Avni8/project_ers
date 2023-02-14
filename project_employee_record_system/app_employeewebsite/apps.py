06b8bdb0145b93e6818c820391b921909e1ca630
Avni8 <avnighimire2000@gmail.com>
2023-02-14 14:19:44 +0545

    project and app initialized

--- project_employee_record_system/app_employeewebsite/apps.py
+++ project_employee_record_system/app_employeewebsite/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppErsConfig(AppConfig):
+class AppEmployeewebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_ers'
+    name = 'app_employeewebsite'
--- project_employee_record_system/project_employee_record_system/wsgi.py
+++ project_employee_record_system/project_employee_record_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_ers project.
+WSGI config for project_employee_record_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_ers.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_employee_record_system.settings')
 
 application = get_wsgi_application()
