c5b8c287a55c17e335a95ad4912db3b9b6a71a49
Avni8 <avnighimire2000@gmail.com>
2023-02-16 18:52:32 +0545

    master page and employee add-view-edit-show pages created

--- project_employee_record_system/app_employeewebsite/urls.py
+++ project_employee_record_system/app_employeewebsite/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('demo/', views.demo, name = 'demo'),
-    path('', views.demo, name = 'demo'),
+    path('employees/', views.employee_index, name = 'emp-index'),
+    path('employees/show/', views.employee_show, name='emp-show'),
+    path('employees/edit/', views.employee_edit, name='emp-edit'),
+    path('employees/add/', views.employee_add, name='emp-add')
 ]
