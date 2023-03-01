a35a308ca3ab74f5cf04459cc9b7b9727868060c
Avni8 <avnighimire2000@gmail.com>
2023-03-01 17:50:52 +0545

    admin panel, static files and bootstrap theme

--- project_employee_record_system/project_employee_record_system/settings.py
+++ project_employee_record_system/project_employee_record_system/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -135,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'avnighimire2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'ciyrutmbdbwxeice'
+EMAIL_HOST_PASSWORD = 'abcdefghisdgafsdh'
 EMAIL_USE_TLS = True
