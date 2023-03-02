76fe0a674f46ccecc6d37c1941684af51e386c86
Avni8 <avnighimire2000@gmail.com>
2023-03-02 17:22:15 +0545

    search and filter data

--- project_employee_record_system/app_employeewebsite/views.py
+++ project_employee_record_system/app_employeewebsite/views.py
@@ -17,7 +17,12 @@ def employee_index(request):
     """ Returns list of employee as context """
 
     employee_list = Employee.objects.all()
+
     context = {"data": employee_list}
+    if request.method == "POST":
+        dataList = Employee.objects.filter(full_name=request.POST.get('full_name'))
+        context.update ({"data": dataList})
+        return render(request, 'employees/index_employee.html', context)
 
     return render(request, 'employees/index_employee.html', context)
 
